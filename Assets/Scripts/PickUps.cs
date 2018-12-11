7fbad547f3646d220cfde0e94e44f531a3adfb15
MHadley98 <43067353+MHadley98@users.noreply.github.com>
2018-12-11 16:46:43 +0000

    Added Assets

--- Assets/Scripts/PickUps.cs
+++ Assets/Scripts/PickUps.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickUps : MonoBehaviour {
+public class PickUps : MonoBehaviour
+{
+    public Rigidbody2D physicsBody;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
