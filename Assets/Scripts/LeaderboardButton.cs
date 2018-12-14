cb57f90de0e15c1d0df4f15c58e525e976d02449
MHadley98 <43067353+MHadley98@users.noreply.github.com>
2018-12-14 16:18:57 +0000

    Added Button functionallity

---
+++ Assets/Scripts/LeaderboardButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Extra using statment to allow us to use the scene management functions
+using UnityEngine.SceneManagement;
+
+
+public class LeaderboardButton : MonoBehaviour
+{
+
+    //called when menu button is pressed
+    public void ToLeaderboard()
+    {
+        //return to menu scene
+        SceneManager.LoadScene("Leaderboard");
+
+    }
+
+}
